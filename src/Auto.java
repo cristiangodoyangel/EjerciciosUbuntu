e590e001556137485ef2be607ed0a7d363ec23ca
cristiangodoyangel <creacciones@gmail.com>
2024-10-29 23:11:21 -0300

    se agregan getter y setters

--- src/Auto.java
+++ src/Auto.java
@@ -1,12 +1,38 @@
 public class Auto {
-    String marca;
-    String modelo;
-    String color;
+    private String marca;
+    private String modelo;
+    private String color;
 
     // Constructor de la clase Auto
     public Auto(String marca, String modelo, String color) {
         this.marca = marca;
         this.modelo = modelo;
+        this.color = color;
+    }
+
+    // Métodos getter
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    // Métodos setter
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setColor(String color) {
         this.color = color;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,19 @@ public class Main {
     public static void main(String[] args) {
         // Crear instancias de la clase Auto
         Auto auto1 = new Auto("Hyundai", "Accent", "Blanco");
-        Auto auto2 = new Auto("Fiaat", "Uno Way", "Blanco");
+        Auto auto2 = new Auto("Fiat", "Uno Way", "Blanco");
 
-        // Imprimir los atributos de cada auto
-        System.out.println("Auto 1: " + auto1.marca + " " + auto1.modelo + " " + auto1.color);
-        System.out.println("Auto 2: " + auto2.marca + " " + auto2.modelo + " " + auto2.color);
+        // Imprimir los atributos de cada auto usando getters
+        System.out.println("Auto 1: " + auto1.getMarca() + " " + auto1.getModelo() + " " + auto1.getColor());
+        System.out.println("Auto 2: " + auto2.getMarca() + " " + auto2.getModelo() + " " + auto2.getColor());
+
+        // Cambiar los atributos de auto1 usando setters
+        auto1.setMarca("Ferrari");
+        auto1.setModelo("Lambo");
+        auto1.setColor("Amarillo Bumblebee");
+
+        // Imprimir los atributos de auto1 después de cambiarlos
+        System.out.println("Auto 1: " + auto1.getMarca() + " " + auto1.getModelo() + " " + auto1.getColor());
     }
 }
+
